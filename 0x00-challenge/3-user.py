415a8fd93be9e8fa7045306f57def6dce4ffc9d4
Brandixitor <med.salah.bourouba@gmail.com>
2021-04-09 13:58:55 +0100

    Fix

--- 0x00-challenge/3-user.py
+++ 0x00-challenge/3-user.py
@@ -40,7 +40,7 @@ class User():
         if pwd is None or type(pwd) is not str:
             self.__password = None
         else:
-            self._password = hashlib.md5(pwd.encode()).hexdigest().lower()
+            self.__password = hashlib.md5(pwd.encode()).hexdigest().lower()
 
     def is_valid_password(self, pwd):
         """
@@ -54,7 +54,7 @@ class User():
             return False
         if self.__password is None:
             return False
-        return hashlib.md5(pwd.encode()).hexdigest().lower() == self.__password
+        return hashlib.md5(pwd.encode()).hexdigest() == self.__password
 
 
 if __name__ == '__main__':
